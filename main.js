2e38c3f63a03849169527a1b3e14a45a92c7cd82
Hector Davis <hector.davis94@gmail.com>
2022-03-24 22:25:02 -0300

    prevent quit on close

--- main.js
+++ main.js
@@ -54,6 +54,14 @@ app.on('ready', () => {
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
 
+  mainWindow.on('close', (e) => {
+    if (!app.isQuitting) {
+      e.preventDefault();
+      mainWindow.hide();
+    }
+    return true;
+  });
+
   const icon = path.join(__dirname, 'assets', 'icons', 'tray_icon.png');
   tray = new Tray(icon);
   tray.on('click', () => {
@@ -62,6 +70,18 @@ app.on('ready', () => {
     } else {
       mainWindow.show();
     }
+  });
+  tray.on('right-click', () => {
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: 'Quit',
+        click: () => {
+          app.isQuitting = true;
+          app.quit();
+        },
+      },
+    ]);
+    tray.popUpContextMenu(contextMenu);
   });
 });
 
